7a4493e5de92281580665b6a60382d893efa4960
martin-boettcher <martin.boettcher@brockmann-consult.de>
2026-01-29 11:07:45 +0100

    set job title

--- force_level2_openeo_test.py
+++ force_level2_openeo_test.py
@@ -15,6 +15,6 @@ datacube = connection.datacube_from_process(
     stac_root="catalogue.json",
 )
 
-job = datacube.create_job()
+job = datacube.create_job(title=__file__)
 job.start_and_wait()
 results = job.get_results()
