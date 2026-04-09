1268c4117b0ceb4fbf4389050248ad377c074594
Hannes Neuschmidt <hannes.neuschmidt@brockmann-consult.de>
2026-04-09 13:29:33 +0200

    rename process to query_stac

--- custom_processes_force.py
+++ custom_processes_force.py
@@ -5,7 +5,9 @@ from openeo_driver.ProcessGraphDeserializer import non_standard_process, Process
 from openeo_driver.processes import ProcessArgs
 from openeo_driver.utils import EvalEnv
 from openeo_driver.util.geometry import BoundingBox
-from openeogeotrellis.load_stac import _spatiotemporal_extent_from_load_params, construct_item_collection
+from openeogeotrellis.load_stac import _spatiotemporal_extent_from_load_params, construct_item_collection, ItemCollection
+
+import pystac
 
 logger = logging.getLogger("FORCE_custom_Processes")
 
@@ -35,11 +37,50 @@ STAC_COLLECTION_URLS = {
 
 @non_standard_process(
     ProcessSpec(
-        id="force_query",
-        description="Returns a list of S3 paths"
-        ).param(name="id", description="Collection to load", schema={"type": "string"}, required=True)
+        id="query_stac",
+        description="Returns an item collection"
+        ).param(name="url", description="URL to the stac collection", schema={"type": "string"}, required=True)
         .param(name="temporal_extent", description="The date range", schema=temporal_extent_schema, required=True)
         .param(name="spatial_extent", description="Area of interest", schema={'type': 'object', 'subtype': 'geojson'}, required=True)
+    .returns(description="List of outputs", schema={
+        "type": "array",
+        "items": {
+            "type": "string"
+        }
+    })
+)
+def query_stac(args: ProcessArgs, env: EvalEnv):
+    collection_url = args.get_required(
+        "url",
+    )
+
+    temporal_extent = None
+    spatial_extent = None
+    if "temporal_extent" in args:
+        temporal_extent = _extract_temporal_extent(
+            args, field="temporal_extent", process_id="force_query"
+        )
+    if "spatial_extent" in args:
+        spatial_extent = _extract_bbox_extent(
+            args, field="spatial_extent", process_id="force_query"
+        )
+
+    items = force_query_stac_catalog(
+        url=collection_url,
+        spatial_extent=spatial_extent,
+        temporal_extent=temporal_extent,
+    )
+    item_collection_pystac = pystac.ItemCollection(items.items)
+    return item_collection_pystac.to_dict()
+
+
+@non_standard_process(
+    ProcessSpec(
+        id="force_query",
+        description="Returns an item_collection"
+    ).param(name="id", description="Collection to load", schema={"type": "string"}, required=True)
+    .param(name="temporal_extent", description="The date range", schema=temporal_extent_schema, required=True)
+    .param(name="spatial_extent", description="Area of interest", schema={'type': 'object', 'subtype': 'geojson'}, required=True)
     .returns(description="List of outputs", schema={
         "type": "array",
         "items": {
@@ -73,16 +114,15 @@ def force_query(args: ProcessArgs, env: EvalEnv):
         temporal_extent=temporal_extent,
     )
 
-    links = [l.target for i in items for l in i[0].links if l.rel == "enclosure"]
+    urls = extract_urls_from_item_collection(items)
 
-    return links
-
+    return urls
 
 def force_query_stac_catalog(
-    url: str,
-    spatial_extent: Union[Dict, BoundingBox, None],
-    temporal_extent: Tuple[Optional[str], Optional[str]],
-    ):
+        url: str,
+        spatial_extent: Union[Dict, BoundingBox, None],
+        temporal_extent: Tuple[Optional[str], Optional[str]],
+    ) -> ItemCollection:
     spatiotemporal_extent = _spatiotemporal_extent_from_load_params(
         spatial_extent=spatial_extent,
         temporal_extent=temporal_extent
@@ -93,11 +133,17 @@ def force_query_stac_catalog(
         spatiotemporal_extent=spatiotemporal_extent,
         property_filter_pg_map=property_filter_pg_map,
     )
+    logger.info(f"Query to '{url}' with spatial_extent '{spatial_extent}' and temporal_extent '{temporal_extent}'")
+    return item_collection
 
-    items = list(item_collection.iter_items_with_band_assets())
-    logger.info(f"Query to '{url}' with spatial_extent '{spatial_extent}' and temporal_extent '{temporal_extent}' returned '{len(items)}' results.")
-    return items
-
+def extract_urls_from_item_collection(item_collection: ItemCollection):
+    items = item_collection.iter_items_with_band_assets()
+    items_list = list(items) # TODO remove
+    logger.info(f"{type(items_list[0][0])=}")
+    items = items_list
+    links = [l.target for i in items for l in i[0].links if l.rel == "enclosure"]
+    logger.info(f"Query returned '{len(links)}' results.")
+    return links
 
 
 
