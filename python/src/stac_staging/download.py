ddacb4a523cd88c7ab62afb35ba76c046aff8040
Hannes Neuschmidt <hannes.neuschmidt@brockmann-consult.de>
2026-04-15 14:49:17 +0200

    Implement fallback if `enclosure` link is not provided
    Older Sentinel-2 L1C STAC items on CDSE do not include the enclosure link type. However, the entire SAFE archive can still be downloaded by guessing the correct url.
    
    Example: https://stac.dataspace.copernicus.eu/v1/collections/sentinel-2-l1c/items/S2B_MSIL1C_20200407T113319_N0500_R080_T28PDC_20230409T205249

--- python/src/stac_staging/download.py
+++ python/src/stac_staging/download.py
@@ -23,6 +23,12 @@ S3_ENDPOINT_URL = "S3_ENDPOINT_URL"
 def download_by_asset(
     items: Iterable[pystac.Item], output_path: Path, synchronous: Optional[bool] = False
 ):
+    """
+    Download all assets of the items in ``items``.
+    :param items: Iterable of items for which to download the assets
+    :param output_path: Base directory where assets should be placed
+    :param synchronous: Whether to download synchronously, one by one. (Default: ``False``).
+    """
     output_path.mkdir(parents=True, exist_ok=True)
     config = stac_asset.Config()
     if synchronous:
@@ -38,6 +44,14 @@ def download_recursive(
     output_path: Path,
     include_toplevel: Optional[bool] = True,
 ):
+    """
+    Download the folder structures described by the items using the "recursive" strategy, based on ``s5cmd``'s cp command.
+    Downloads the S3 key provided in the assets and all sub-keys.
+    :param items: Iterable of items for which to download the assets
+    :param output_path: base path where assets should be placed
+    :param include_toplevel: Whether to include the toplevel key (e.g. the SAFE archive name) in the output file structure.
+        if ``False``, assets from all items will be placed in the same directory, depending on the (default True).
+    """
     if shutil.which(S5CMD) is None:
         raise RuntimeError(
             "Recursive download depends on s5cmd. Make sure it is installed."
@@ -68,18 +82,34 @@ def download_recursive(
 def _download_assets_sync(
     assets: Dict, output_path_base: Path, config: stac_asset.Config
 ):
+    """
+    Download assets asynchronously to ``output_path_base/local/path`` where ``local/path`` is determined by
+    the ``file:local_path`` attribute of the asset. Fails if ``file:local_path`` is not set.
+    Unused, useful for debugging downloads.
+
+    :param assets: Mapping of asset keys to ``pystac.Asset``s
+    :param output_path_base: assets will be downloaded to subpaths of this path
+    :param config: stac_asset Configuration
+    """
     for asset_key, asset in assets.items():
         download_path = (
             output_path_base / asset.extra_fields["file:local_path"]
         ).resolve()
         LOGGER.info(f"Downloading {asset_key} to {download_path} (sync)")
-        # TODO async download
         blocking.download_asset(asset_key, asset, path=download_path, config=config)
 
 
 async def _download_assets_async(
     assets: Dict, output_path_base: Path, config: stac_asset.Config
 ):
+    """
+    Download assets asynchronously to ``output_path_base/local/path`` where ``local/path`` is determined by
+    the ``file:local_path`` attribute of the asset. Fails if ``file:local_path`` is not set.
+
+    :param assets: Mapping of asset keys to ``pystac.Asset``s
+    :param output_path_base: assets will be downloaded to subpaths of this path
+    :param config: stac_asset Configuration
+    """
     async with aiohttp.ClientSession() as session:
         async with asyncio.TaskGroup() as tg:
             client_cache = Clients(
@@ -105,17 +135,45 @@ async def _download_assets_async(
                 )
 
 
-def _extract_enclosure_link_url(item: pystac.Item) -> Optional[str]:
+def _extract_enclosure_link_url(item: pystac.Item) -> str:
+    """
+    Determine the S3 url of the enclosing SAFE archive. If a link with
+
+    :param item:
+    :return:
+    """
     enclosure_links = set(l.href for l in item.links if l.rel == "enclosure")
 
-    if len(enclosure_links) != 1:
+    if len(enclosure_links) > 1:
+        raise ValueError(f"Item {item} contains more than one enclosure link")
+    if len(enclosure_links) == 0:
         LOGGER.warning(
-            f"Item {item} contains something other than a single enclosure link: '{enclosure_links}', skipping."
+            f"Item {item} contains no enclosure_link, using fallback method to determine s3 url."
         )
-        return None
+        enclosure_links = {_construct_enclosure_link_from_item(item)}
+        LOGGER.info(f"Fallback found enclosure {enclosure_links}")
 
     url = enclosure_links.pop()
     LOGGER.info(f"Found enclosure link: '{url}' for item '{item}'")
+    return url
+
+
+def _construct_enclosure_link_from_item(item: pystac.Item) -> str:
+    """
+    Guesses the S3 url of the enclosing SAFE archive from the location of the manifest.safe metadata file.
+    Fails (ValueError), if there is no "safe_manifest" asset in the item.
+    This function assumes that the file structure of the SAFE archive is preserved on the remote storage
+    and may produce incorrect urls if this is not the case.
+
+    :param item: Item for which to determine the location of the SAFE archive
+    :return: s3 url to the enclosing SAFE archive
+    """
+    if "safe_manifest" in item.assets:
+        # href: s3://eodata/Sentinel-2/MSI/L1C/2026/04/15/S2A_MSIL1C_20260415T085441_N0512_R107_T37VEC_20260415T105317.SAFE/manifest.safe
+        url = item.assets["safe_manifest"].href.removesuffix("manifest.safe")
+    else:
+        raise ValueError(f"No 'safe_manifest' in {item}")
+
     return url
 
 
