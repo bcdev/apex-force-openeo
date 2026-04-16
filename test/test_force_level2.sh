45e0914e878e54ea7bb039d4ac293ed0f44d150a
Hannes Neuschmidt <hannes.neuschmidt@brockmann-consult.de>
2026-04-16 10:25:13 +0200

    Add ability to build docker image in test. Do not log secrets

--- test/test_force_level2.sh
+++ test/test_force_level2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -x
+set -euxo pipefail
 repo_root=$(realpath "$(dirname "$0")/..")
 echo "repo_root: ${repo_root}"
 
@@ -7,10 +7,20 @@ export AWS_ENDPOINT_URL_S3='https://eodata.dataspace.copernicus.eu'
 export S3_ENDPOINT_URL="$AWS_ENDPOINT_URL_S3" # for s5cmd
 
 env_message="Please make sure AWS_SECRET_ACCESS_KEY and AWS_ACCESS_KEY_ID are set when running this test (${0})"
+docker_image_name="quay.io/bcdev/force-eoap:dev"
 
+set +x # do not log credentials
 if [[ -z "$AWS_SECRET_ACCESS_KEY" || -z "$AWS_ACCESS_KEY_ID" ]]; then
   echo "$env_message" >&2
   exit 1
+fi
+set -x
+
+if [[ "$1" == "docker" ]]; then
+  echo "Building docker container ${docker_image_name}"
+  docker build -t "$docker_image_name" "$repo_root"
+else
+  echo "Reusing docker image ${docker_image_name}. Pass 'docker' as an argument to rebuild it"
 fi
 
 #TODO replace l2 with l2 workflow
