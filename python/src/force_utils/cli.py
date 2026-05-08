055f87dd41a4f1e7ab2a8227349ae322090d4124
Hannes Neuschmidt <hannes.neuschmidt@brockmann-consult.de>
2026-05-08 10:52:06 +0200

    Validate all if validation flag is passed

--- python/src/force_utils/cli.py
+++ python/src/force_utils/cli.py
@@ -64,7 +64,7 @@ def gen_stac(datacube_root, output_path, item_id, type, parameter_path, validate
     catalog = stac_builder.generate_stac(item_id=item_id)
     catalog.normalize_hrefs(str(output_path))
     if validate:
-        catalog.validate()
+        catalog.validate_all()
     logger.info(f"Saving STAC catalog to {output_path.resolve()}")
     catalog.save(
         dest_href=str(output_path), catalog_type=pystac.CatalogType.SELF_CONTAINED
